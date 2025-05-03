edb78aa07c02dfc23dc980169a9687cacb74aeab
light483-l <imlight483@gmail.com>
2025-05-04 02:05:27 +0300

    Update mapapi_PG.py

--- mapapi_PG.py
+++ mapapi_PG.py
@@ -4,19 +4,15 @@ import os
 
 
 def show_map(ll_spn, map_type="map", add_params=None):
-    """Отображение карты с помощью pygame"""
     map_url = "https://static-maps.yandex.ru/1.x/"
 
-    # Базовые параметры
     params = {
         "l": map_type,
         "size": "650,450"
     }
-
-    # Добавляем координаты и масштаб
+    
     params.update(dict(param.split('=') for param in ll_spn.split('&')))
 
-    # Добавляем дополнительные параметры (метку)
     if add_params:
         params.update(dict(param.split('=') for param in add_params.split('&')))
 
@@ -24,18 +20,15 @@ def show_map(ll_spn, map_type="map", add_params=None):
         response = requests.get(map_url, params=params)
         response.raise_for_status()
 
-        # Сохраняем временный файл карты
         map_file = "map.png"
         with open(map_file, "wb") as f:
             f.write(response.content)
 
-        # Инициализация pygame
         pygame.init()
         screen = pygame.display.set_mode((650, 450))
         screen.blit(pygame.image.load(map_file), (0, 0))
         pygame.display.flip()
 
-        # Ожидание закрытия окна
         running = True
         while running:
             for event in pygame.event.get():
